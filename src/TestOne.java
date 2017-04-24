3932834cd40954681e70419476d86cb2252faec4
Sebastian <arkan382382@outlook.com>
2017-04-24 14:57:31 +0200

    bicycle inheritance

--- src/TestOne.java
+++ src/TestOne.java
@@ -8,19 +8,40 @@ public class TestOne {
         //problem with github updates
 
         Bicycle firstBicycle = new Bicycle();
-        firstBicycle.SetGear(2);
-        firstBicycle.ShowDetails();
+        firstBicycle.setGear(2);
+        firstBicycle.showDetails();
 
+        MountainBike secondBicycle = new MountainBike();
+        secondBicycle.setGear(3);
+        secondBicycle.setVelocity(50);
+        secondBicycle.setTirePresure(2);
+        secondBicycle.showDetails();
+        secondBicycle.showTirePresure();
     }
 }
 
 class Bicycle{
     private int gear;
+    private int velocity;
 
-    void SetGear(int val){
+    void setGear(int val){
         gear = val;
     }
-    void ShowDetails(){
-        System.out.print(gear);
+    void setVelocity(int val2){
+        velocity = val2;
+    }
+    void showDetails(){
+        System.out.print(gear + " " + velocity);
+    }
+}
+
+class MountainBike extends Bicycle{
+    private int tirePresure;
+
+    void setTirePresure(int val3){
+        tirePresure = val3;
+    }
+    void showTirePresure(){
+        System.out.print(tirePresure);
     }
 }
